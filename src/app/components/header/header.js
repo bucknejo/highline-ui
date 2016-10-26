d5c37ba0178f6326cf94ca453912c9063c6d4a0b
jbuckner <jon@redbonesolutions.com>
2016-10-26 09:23:27 -0400

    Direct changes into master for wholesale project retrofit

---
+++ src/app/components/header/header.js
@@ -0,0 +1,11 @@
+function HighlineHeaderController() {
+
+    console.log('in HighlineHeaderController');
+
+}
+
+angular.module('highline-ui').component('highlineHeader', {
+    templateUrl: 'app/components/header/header.tpl.html',
+    controller: HighlineHeaderController,
+    bindings: {}
+});
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,23 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var highline = angular.module('highline-ui', ['ui.router']);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+highline.config(['$stateProvider', function($stateProvider) {
+
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>Hello!</h3>'
+  }
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>This is a UI Router-enabled app!</h3>'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+
 }]);
